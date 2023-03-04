b7e88f75eb407976912f6e45e0ef3c9a4edf1090
mk solutions <mohamedsaleh445@gmail.com>
2023-03-04 16:38:47 +0200

    1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.log("hello from index")
+var userInfo = {
+    name : "saleh",
+    age : "32",
+}
+var mainInfo = {
+    followers : 77,
+    following : 234,
+}
+
+var secondInfo ={
+    location : "egypt",
+    link : {
+        tital :"github",
+        href : "",
+    }
+
+}
